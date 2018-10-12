0f5a62b893e54be068718592f10b5c69a9fd73b8
akeele <aksu.suunta@gmail.com>
2018-10-12 13:02:25 +0300

    Added _get_daily_returns routine

--- data_reader.py
+++ data_reader.py
@@ -12,7 +12,7 @@ class DatastreamCsvPriceHandler(object):
         """
         self.csv_file = csv_file
         self.daily_prices = self._get_daily_prices(self)
-
+        self.daily_returns = self._get_daily_returns(self.daily_prices)
 
     def _read_csv(self, csv_file):
         """
@@ -30,7 +30,9 @@ class DatastreamCsvPriceHandler(object):
                 # Start collecting price information
                 if is_price_series:
                     date = fields[0]
-                    price = fields[1].strip()
+                    # Strip newline from the end and convert comma to dot ie. '92,06\n' -> '92.06'
+                    price = fields[1].strip().replace(",", ".")
+                    price = float(price)
                     # Convert date to datetime object
                     date = datetime.strptime(date, "%d.%m.%Y")
                     # Collect dates and prices to dictionary
@@ -44,3 +46,23 @@ class DatastreamCsvPriceHandler(object):
         price_series = self._read_csv(self)
         daily_prices = numpy.array(list(price_series.items()))
         return daily_prices
+
+    def _get_daily_returns(self, daily_prices):
+        """
+        Return daily percentage returns
+        """
+        DATE = 0
+        PRICE = 1
+        daily_returns = []
+        # Can't compute for first day of price series, so start at second bar
+        for idx in range(1, len(daily_prices)):
+            # contains date and return
+            one_day = []
+            previous_day = daily_prices[idx-1]
+            current_day = daily_prices[idx]
+            # Calculates percentage return
+            daily_return = (current_day[PRICE] - previous_day[PRICE])/previous_day[PRICE]
+            one_day.append(current_day[DATE])
+            one_day.append(daily_return)
+            daily_returns.append(one_day)
+        return numpy.array(daily_returns)
