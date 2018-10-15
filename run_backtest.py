f2b75ccfa4efea44ea80013ab4f2fdf08bc9afe3
akeele <aksu.suunta@gmail.com>
2018-10-15 14:47:22 +0300

    Fixed regime filter backtest

--- run_backtest.py
+++ run_backtest.py
@@ -1,21 +1,32 @@
-from train_hmm import RegimeHmmModel
-from data_reader import DatastreamCsvPriceHandler
-
+import numpy
 
 class Backtest(object):
     """
     Create backtest
     """
 
-    def __init__(self, daily_prices, strategy, hmm_model, start_date, end_date):
+    def __init__(self, daily_prices, daily_returns, strategy, hmm_model, start_date, end_date):
         """
         Takes strategy, which defines the strategy, start date and end date of backtest
         """
         self.daily_prices = daily_prices
+        self.daily_returns = daily_returns
         self.strategy = strategy
-        self.strategy = hmm_model
+        self.hmm_model = hmm_model
         self.start_date = start_date
         self.end_date = end_date
+
+    def _get_index_before_backtest(self, daily_returns):
+        """
+        Returns index of date just before backtest start date
+        """
+        DATE = 0
+        end = 0
+        for idx, day in enumerate(daily_returns):
+            if day[DATE] == self.start_date:
+                end = idx - 1
+
+        return end
 
     def _get_backtest_period(self, daily_prices, start_date, end_date):
         """
@@ -34,24 +45,25 @@ class Backtest(object):
 
         return daily_prices[start:end]
 
-    def _check_regime(self, observed_price_series):
+    def _check_regime(self, observed_return_series):
         """
         Check current volatility regime by finding the most probable state of underlying Markov chain with Viterbi algorithm
         """
-        current_regime = self.hmm_model.predict(observed_price_series)[-1]
-        if current_regime = 0:
+        observed_return_series = numpy.column_stack([observed_return_series[:,1]])
+        current_regime = self.hmm_model.predict(observed_return_series)[-1]
+        if current_regime == 0:
             return "low"
         else:
             return "high"
 
-    def _buy_and_hold(backtest_period_daily_prices, portfolio):
+    def _buy_and_hold(self, backtest_period_daily_prices, portfolio):
         """
         Performs buy-and-hold investment strategy
         """
         for day in backtest_period_daily_prices:
             portfolio.append(day)
 
-    def _regime_filter_strategy(backtest_period_daily_prices, portfolio):
+    def _regime_filter_strategy(self, backtest_period_daily_prices, portfolio):
         """
         Performs buy-and-hold strategy with HMM regime filter. If regime is 'low' – meaning low volatility – we go long or maintain
         our long position. If it's 'high' we sell or do nothing if already not invested.
@@ -59,32 +71,48 @@ class Backtest(object):
         DATE = 0
         PRICE = 1
         is_invested = True
+        before_backtest_index = self._get_index_before_backtest(self.daily_returns)
+        # This is 1 in the beginning, but increases when portfolio outperforms and vice versa
+        number_of_shares = 1
         for idx, day in enumerate(backtest_period_daily_prices):
+            # Currently observed historical returns
+            observed_return_series = self.daily_returns[:(before_backtest_index+idx)]
+            current_regime = self._check_regime(observed_return_series)
+            day = numpy.append(day, current_regime)
             # Start invested
             if day[DATE] == self.start_date:
                 portfolio.append(day)
                 continue
             else:
                 current_portfolio_value = portfolio[-1][PRICE]
-                # This is 1 in the beginning, but increases when portfolio outperforms and vice versa
-                number_of_shares = day[PRICE] / current_portfolio_value
                 previous_day = idx - 1
-                current_regime = self._check_regime(backtest_period_daily_prices[:previous_day])
+                #print(current_regime)
                 if current_regime == "low":
+                    if not is_invested:
+                        number_of_shares = current_portfolio_value / day[PRICE]
+                        new_portfolio_value = day[PRICE] * number_of_shares
+                        day[PRICE] = new_portfolio_value
+                        #new_day = [day[DATE], new_portfolio_value]
+                        portfolio.append(day)
+                        is_invested = True
                     # Calculate new value based on amount of shares
-                    new_portfolio_value = day[PRICE] * number_of_shares
-                    new_day = [day[DATE], new_portfolio_value]
-                    portfolio.append(new_day)
-                    is_invested = True
+                    else:
+                        new_portfolio_value = day[PRICE] * number_of_shares
+                        day[PRICE] = new_portfolio_value
+                        #new_day = [day[DATE], new_portfolio_value]
+                        portfolio.append(day)
+                        is_invested = True
                 elif current_regime == "high":
                     if is_invested:
                         # If invested, we sell. So new portfolio value is the same as previous day
-                        new_day = [day[DATE], current_portfolio_value]
-                        portfolio.append(new_day)
+                        day[PRICE] = current_portfolio_value
+                        #new_day = [day[DATE], current_portfolio_value]
+                        portfolio.append(day)
                         is_invested = False
                     else:
-                        new_day = [day[DATE], current_portfolio_value]
-                        portfolio.append(new_day)
+                        day[PRICE] = current_portfolio_value
+                        #new_day = [day[DATE], current_portfolio_value]
+                        portfolio.append(day)
 
     def run(self):
         """
@@ -96,9 +124,9 @@ class Backtest(object):
         # Check which strategy
         if self.strategy == "buy-and-hold":
             # Loop through all dates in backtest period
-            _buy_and_hold(backtest_period_daily_prices, portfolio)
+            self._buy_and_hold(backtest_period_daily_prices, portfolio)
             return portfolio
 
-        elif self.strategy == "regime-model":
-            _regime_filter_strategy(backtest_period_daily_prices, portfolio)
+        elif self.strategy == "regime-filter":
+            self._regime_filter_strategy(backtest_period_daily_prices, portfolio)
             return portfolio
