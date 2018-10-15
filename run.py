198720067ca9e73cf139bb0d98684412acfe2342
akeele <aksu.suunta@gmail.com>
2018-10-15 14:46:27 +0300

    Working model

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from run_backtest import Backtest
 from data_reader import DatastreamCsvPriceHandler
 from train_hmm import RegimeHmmModel
+
+import sys
 import pickle
+import numpy
 from datetime import datetime
 import argparse
 
@@ -20,7 +23,7 @@ def get_training_data(daily_returns, start_date, end_date):
             end = idx
             break
 
-    return daily_returns[start:end]
+    return numpy.column_stack([daily_returns[start:end, 1]])
 
 if __name__ == "__main__":
 
@@ -30,21 +33,36 @@ if __name__ == "__main__":
     argparser.add_argument("--backtest-end", default="31.12.2017", help="End date of backtest")
     argparser.add_argument("--training-start", default=None, help="Start date of training period")
     argparser.add_argument("--training-end", default=None, help="End date of training period")
+    argparser.add_argument("--strategy", default=None, help="Investment strategy to backtest. Currently supports 'buy-and-hold' and 'regime-filter'")
     argparser.add_argument("--pickle-path", default=None, help="File to store trained HMM")
-
-
     args = argparser.parse_args()
 
+    if args.strategy not in ["buy-and-hold", "regime-filter"]:
+        print("That strategy isn't on the list of accepted strategies")
+        sys.exit()
+
     # Get daily prices and returns, as numpy arrays with shape (days, 2). First column is dates, second prices or percentage returns.
-    price_handler = DatastreamCsvPriceHandler(args._csv_file)
+    price_handler = DatastreamCsvPriceHandler(args.csv_file)
     daily_prices = price_handler.daily_prices
     daily_returns = price_handler.daily_returns
 
     # Train HMM
-    start_date = datetime.strptime(args._training_start, '%d.%m.%Y')
-    end_date = datetime.strptime(args._training_end, '%d.%m.%Y')
-    training_data = get_training_data(daily_returns, start_date, end_date)
-    RegimeHmmModel(training_data, n_states=2, n_iters=1000, pickle_path=args._pickle_path)
+    training_start_date = datetime.strptime(args.training_start, '%d.%m.%Y')
+    training_end_date = datetime.strptime(args.training_end, '%d.%m.%Y')
+    training_data = get_training_data(daily_returns, training_start_date, training_end_date)
+    RegimeHmmModel(training_data, n_states=2, n_iters=100000, pickle_path=args.pickle_path)
 
     # Run backtest
-    
+    backtest_start_date = datetime.strptime(args.backtest_start, '%d.%m.%Y')
+    backtest_end_date = datetime.strptime(args.backtest_end, '%d.%m.%Y')
+    trained_hmm_model = pickle.load(open(args.pickle_path, 'rb'))
+    #print(trained_hmm_model.transmat_)
+    #for i in range(trained_hmm_model.n_components):
+    #    print("Mean of state: ", trained_hmm_model.means_[i])
+    #    print("Variance of state: ", trained_hmm_model.covars_[i])
+    strategy = args.strategy
+    backtest = Backtest(daily_prices, daily_returns, strategy, trained_hmm_model, backtest_start_date, backtest_end_date)
+    backtested_portfolio = backtest.run()
+
+    for item in backtested_portfolio:
+        print(item[1:])
